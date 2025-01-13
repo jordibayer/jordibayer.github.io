24e272b4178d3313c3bd644bf2af3a1cc9011279
Jordi <jordi.be.1992@gmail.com>
2025-01-13 19:55:35 +0100

    Updated the cv

--- src/helpers/yearFormat.ts
+++ src/helpers/yearFormat.ts
@@ -1,4 +1,4 @@
-export default function yearFormat(startDate: string, endDate: string): string {
+export default function yearFormat(startDate: string, endDate: string | undefined): string {
   const startYear = new Date(startDate).getFullYear();
   const endYear = endDate != null ? new Date(endDate).getFullYear() : "Actual";
   const years = `${startYear} - ${endYear}`;
